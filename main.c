49b80df41514f3831a481796a8d8374ddb69ada8
Vadim Pandrak <vpandrak@gmail.com>
2024-11-23 18:41:17 +0500

    Первоначальный коммит, программа для определения расстояния между точками, точки представлены целыми числами

---
+++ main.c
@@ -0,0 +1,19 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <math.h>
+
+int main() {
+    //настройка кодировки
+    system("chcp 1251");
+    system("cls");
+    int x1, y1, x2, y2;
+    printf("Введите координаты первой точки (x1, y1): ");
+    scanf("%d %d", &x1, &y1);
+    printf("Введите координаты второй точки (x2, y2): ");
+    scanf("%d %d", &x2, &y2);
+
+    double distance = sqrt(pow(x2 - x1, 2) + pow(y2 - y1, 2));
+    printf("Расстояние: %.2f\n", distance);
+    getchar(); getchar();
+    return 0;
+}
