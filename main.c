ab4a854aa17ee623e0ebddcf064c697694e7ce88
Vadim Pandrak <vpandrak@gmail.com>
2024-11-24 11:11:31 +0500

    Добавление комментария

--- main.c
+++ main.c
@@ -3,17 +3,17 @@
 #include <math.h>
 
 int main() {
-    // 
+    //кодировка
     system("chcp 1251");
     system("cls");
     double x1, y1, x2, y2;
-    printf("    (x1, y1): ");
+    printf("Введите первую точку (x1, y1): ");
     scanf("%lf %lf", &x1, &y1);
-    printf("    (x2, y2): ");
+    printf("Введите вторую точку (x2, y2): ");
     scanf("%lf %lf", &x2, &y2);
-
+    //алгоритм расчета
     double distance = sqrt(pow(x2 - x1, 2) + pow(y2 - y1, 2));
-    printf("   (%lf, %lf)  (%lf, %lf): %.2f\n", x1,y1,x2, y2, distance);
+    printf("Расстояние между двумя точками (%lf, %lf) и (%lf, %lf): %.2f\n", x1,y1,x2, y2, distance);
     getchar(); getchar();
     return 0;
 }
