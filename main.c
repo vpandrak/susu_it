ae4c919455c70fb1f594c3c187f85a4e7c0b20f1
Vadim Pandrak <vpandrak@gmail.com>
2024-11-24 11:20:26 +0500

    изменено имя переменной

--- main.c
+++ main.c
@@ -6,14 +6,14 @@ int main() {
     //настройка кодировки
     system("chcp 1251");
     system("cls");
-    double x1, y1, x2, y2;
+    double x, y1, x2, y2;
     printf("Введите координаты первой точки (x1, y1): ");
-    scanf("%lf %lf", &x1, &y1);
+    scanf("%lf %lf", &x, &y1);
     printf("Введите координаты второй точки (x2, y2): ");
     scanf("%lf %lf", &x2, &y2);
 
-    double distance = sqrt(pow(x2 - x1, 2) + pow(y2 - y1, 2));
-    printf("Расстояние между точками (%lf, %lf) и (%lf, %lf): %.2f\n", x1,y1,x2, y2, distance);
+    double distance = sqrt(pow(x2 - x, 2) + pow(y2 - y1, 2));
+    printf("Расстояние между точками (%lf, %lf) и (%lf, %lf): %.2f\n", x,y1,x2, y2, distance);
     getchar(); getchar();
     return 0;
 }
